c8d77581245b9a4246870933e69a97b4be8a6557
lyz0313 <43334762+lyz0313@users.noreply.github.com>
2019-04-03 22:33:53 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,36 @@
+<!--
+<?php
+
+//造一个mysqli对象，造连接对象
+$db=new MySQLi("localhost","root","aaa052460","test");
+//括号里面填的是IP地址域名，用户名，密码，数据库的名字
+
+if($db->connect_error){
+	echo 'Could not connect to database.';
+	echo "<br/>";
+	exit;
+}
+else{
+	echo  '连接成功';
+	echo "<br/>";
+}
+$sql = "CREATE DATABASE test3";
+if ($db->query($sql) === TRUE) {
+    echo "数据库创建成功";
+} 
+else {
+    echo "Error creating database: " ;
+}
+$db->close();//关闭sql连接
+?>
+-->
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
